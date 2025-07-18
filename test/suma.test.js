c863494ef34d2cc8472f95df9d3d91e4439a512e
Mirian-Carrill <miriancarrillopanzo@gmail.com>
2025-07-18 17:15:56 -0600

    agrego

--- test/suma.test.js
+++ test/suma.test.js
@@ -1,8 +1,8 @@
 const suma = require('../src/suma');
-test('Suma 2 + 3 = 5',() => {
+test('suma 2 + 3 = 5',() => {
     expect(suma(2,3)).toBe(5);
 });
 
-test('Suma -1 +1 = 0',() => {
+test('suma -1 + 1 = 0',() => {
     expect(suma(-1, 1)).toBe(0);
 });
